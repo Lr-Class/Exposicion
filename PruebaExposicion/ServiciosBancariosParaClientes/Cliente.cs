a570bc1d4cd51c560a108b2eb388dd756bd7b14c
Lr-Class <160526764+Lr-Class@users.noreply.github.com>
2024-04-16 13:39:00 -0500

    Add files via upload

---
+++ PruebaExposicion/ServiciosBancariosParaClientes/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiciosBancariosParaClientes
+{
+    public class Cliente
+    {
+        private string nombre { get; set; }
+
+
+
+        public Cliente(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+
+
+        public string ObtenerNombre()
+        {
+            return this.nombre;
+        }
+
+        public void EstablecerNombre(string nuevoNombre)
+        {
+            this.nombre = nuevoNombre;
+        }
+    }
+}
---
+++ PruebaExposicion/ServiciosBancariosParaClientes/CuentaBancaria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiciosBancariosParaClientes
+{
+    public class CuentaBancaria
+    {
+        private double balance { get; set; }
+
+        public CuentaBancaria(double balanceInicial)
+        {
+            this.balance = balanceInicial;
+        }
+
+        public void Depositar(double cantidad)
+        {
+            this.balance += cantidad;
+        }
+
+        public bool Retirar(double cantidad)
+        {
+            if (this.balance >= cantidad)
+            {
+                this.balance -= cantidad;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double ObtenerBalance()
+        {
+            return this.balance;
+        }
+    }
+}
